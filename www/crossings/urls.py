ab860d176fde916036aa7593943329c5c26b1b84
terrycojones <terry@jon.es>
2016-07-09 16:06:33 +0100

    Fiddled URLs to make crossings the default. A bit ugly.

--- www/crossings/urls.py
+++ www/crossings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<crossingId>[0-9]+)/comments\.json$', views.comments, name='comments'),
-    url(r'^(?P<crossingId>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^crossings/(?P<crossingId>[0-9]+)/comments\.json$', views.comments,
+        name='comments'),
+    url(r'^crossings/(?P<crossingId>[0-9]+)/$', views.detail, name='detail'),
 ]
